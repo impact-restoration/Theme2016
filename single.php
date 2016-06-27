be2c5b7429e96d269078477b1817fe827340e9e4
joelworsham <joel.worsham@gmail.com>
2016-06-27 12:23:15 -0400

    Single

--- single.php
+++ single.php
@@ -14,6 +14,22 @@ get_header();
 
 the_post();
 ?>
+	<header class="post-header callout-section black left row expand">
+		<div class="callout-content columns small-12 medium-6 large-5">
+			<div class="container">
+				<h2 class="callout-content-title no-underline"><?php the_title(); ?></h2>
+			</div>
+		</div>
+
+		<div class="callout-prompt columns small-12 medium-6 large-7">
+		</div>
+	</header>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'row' ) ); ?>>
+		<div class="post-content columns small-12">
+			<?php the_content(); ?>
+		</div>
+	</article>
 
 <?php
 get_footer();
